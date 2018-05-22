2ba6d1a5becc60694cf6d4bbbbe1165e4c2a3b73
hansliebert <34358360+hansliebert@users.noreply.github.com>
2018-05-21 20:11:52 -0400

    Add files via upload

---
+++ src/arch/x86_64/multiboot_header.asm
@@ -0,0 +1,18 @@
+section .multiboot_header ; declaring the section name
+
+header_start: ; header_start / _end are labels that mark memory location used to calculate the header length easier
+
+	dd 0xe85250d6							; magic number (multiboot 2)
+	dd 0								; architecture 0 (protected mode i386)
+	dd header_end - header_start					; header length
+	; checksum	
+	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+	
+	; insert optional multiboot tags here
+
+	; required end tag
+	dw 0	; type
+	dw 0	; flags
+	dd 8	; size
+
+header_end:
